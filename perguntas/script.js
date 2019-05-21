5fb220d5c55f5643bb718ac6c57107c391077516
Everton Dias <31146819+Lemon42@users.noreply.github.com>
2019-05-21 18:56:08 -0300

    Mudança de JavaScript integrado para separado (Obs.: O medio.html quebrou por conta de não ter o numero de quetões suficiente)

---
+++ perguntas/script.js
@@ -0,0 +1,55 @@
+
+	 	function checked(){
+			
+			// Desabilitar
+			indice_marcado = 0
+			function deshabilitar(formulario,idradio){
+				formulario.miradio[indice_marcado].checked = true
+				formulario.miradio[idradio].blur()	
+		}
+			
+            // validacao
+            for (var x = 0; x < 2; x++)
+            {
+                if (document.formPergunta[x].radio[0].checked == false && document.formPergunta[x].radio[1].checked == false && document.formPergunta[x].radio[2].checked == false && document.formPergunta[x].radio[3].checked == false)
+                    {
+                        alert('Você não respondeu todas as questões!!! Por favor responda . . .');
+                        return false;
+                    }
+            }
+       
+            // contagem de acertos
+            var acertos = 0;
+            
+			for(var x = 1; x <= 10; x++)
+			{
+				if (document.getElementById("r" + x).checked == true)
+				{
+					acertos = acertos+1;
+				}
+			}
+
+  			sessionStorage.setItem("variavel", acertos);
+			open('fim.html','fim','status=no,Width=700px,Height=600px');
+			
+			// Mudando a cor das respostas
+			for(var i = 1; i <= 10; i++) 
+			{
+				document.getElementById("resposta" + i).style.color = "#00FF7F"; 
+			}
+			
+			// Travando as respostas erradas
+			for(var c = 1; c <= 30; c++)
+			{
+				document.getElementById("input" + c).disabled = true;
+			}
+			
+			// Travando as respostas certas
+			for(var q = 1; q <= 10; q++)
+			{
+				document.getElementById("r" + q).disabled = true;
+			}
+
+			
+			return false;
+        }
