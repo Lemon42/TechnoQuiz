a7226ce9f323ccea987100b5b9cf45cddd2e747b
Everton Dias <31146819+Lemon42@users.noreply.github.com>
2019-05-26 23:35:23 -0300

    Termino do site

--- perguntas/script.js
+++ perguntas/script.js
@@ -1,47 +1,48 @@
+// programado por: Everton Dias e Gabriel Lopes
 
-	 	function checked(){
-			
-            // validacao
-            for (var x = 0; x < 2; x++)
-            {
-                if (document.formPergunta[x].radio[0].checked == false && document.formPergunta[x].radio[1].checked == false && document.formPergunta[x].radio[2].checked == false && document.formPergunta[x].radio[3].checked == false)
-                    {
-                        alert('Você não respondeu todas as questões!!! Por favor responda . . .');
-                        return false;
-                    }
-            }
-       
-            // contagem de acertos
-            var acertos = 0;
-            
-			for(var x = 1; x <= 10; x++)
+function checked()
+{
+	// validacao
+	for (var x = 0; x < 2; x++)
+  {
+		if (document.formPergunta[x].radio[0].checked == false && document.formPergunta[x].radio[1].checked == false && document.formPergunta[x].radio[2].checked == false && document.formPergunta[x].radio[3].checked == false)
+    {
+    	alert('Você não respondeu todas as questões!!! Por favor responda . . .');
+      return false;
+      }
+  }
+
+	// contagem de acertos
+  var acertos = 0;
+
+	for(var x = 1; x <= 10; x++)
+	{
+		if (document.getElementById("r" + x).checked == true)
 			{
-				if (document.getElementById("r" + x).checked == true)
-				{
 					acertos = acertos+1;
-				}
 			}
+	}
 
-  			sessionStorage.setItem("variavel", acertos);
-			open('fim.html','fim','status=no,Width=700px,Height=600px');
-			
-			// Mudando a cor das respostas
-			for(var i = 1; i <= 10; i++) 
-			{
-				document.getElementById("resposta" + i).style.color = "#00FF7F"; 
-			}
-			
-			// Travando as respostas erradas
-			for(var c = 1; c <= 30; c++)
-			{
-				document.getElementById("input" + c).disabled = true;
-			}
-			
-			// Travando as respostas certas
-			for(var q = 1; q <= 10; q++)
-			{
-				document.getElementById("r" + q).disabled = true;
-			}
+  sessionStorage.setItem("acertosInstacia", acertos); // Criando variavel de instância
+	open('fim.html','fim','status=no,Width=730px,Height=600px'); // Abrindo pagina de qtt de resposta
 
-			return false;
-        }
+	// Mudando a cor das respostas
+	for(var i = 1; i <= 10; i++)
+	{
+		document.getElementById("resposta" + i).style.color = "#00FF7F";
+	}
+
+	// Travando as respostas erradas
+	for(var c = 1; c <= 30; c++)
+	{
+		document.getElementById("input" + c).disabled = true;
+	}
+
+	// Travando as respostas certas
+	for(var q = 1; q <= 10; q++)
+	{
+		document.getElementById("r" + q).disabled = true;
+	}
+
+	return false;
+}
